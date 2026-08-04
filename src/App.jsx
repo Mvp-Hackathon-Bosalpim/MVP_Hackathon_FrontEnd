da4db9ebff62e50c5c0a5f58d50816e63033bf4f
Ajungmiiin <wjdals3249@naver.com>
2026-08-05 03:38:03 +0900

    feat : register-page 마크업

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import HomePage from './pages/HomePage'
-import NotFoundPage from './pages/NotFoundPage'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RegisterPage from "./pages/register-page";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomePage />,
   },
   {
-    path: '*',
+    path: "*",
     element: <NotFoundPage />,
   },
-])
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
 
 function App() {
   return (
@@ -23,7 +28,7 @@ function App() {
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
