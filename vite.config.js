35167b26fa4cad29f56e958c756ba2f2c94fa465
Ajungmiiin <wjdals3249@naver.com>
2026-08-09 03:54:35 +0900

    fix : 빌드 에러 수정

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { fileURLToPath, URL } from "node:url";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
@@ -6,24 +6,28 @@ import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BASE_API_URL,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BASE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [
-    tailwindcss(),
-    svgr(),
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [
+      tailwindcss(),
+      svgr(),
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
